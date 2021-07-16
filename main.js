2de803d43d0a6e122cd6305d187ef760acdbe869
ASJ91114 <75980627+ASJ91114@users.noreply.github.com>
2021-07-16 16:48:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+status = "";
+object = [];
+sound = "";
+
+function preload() {
+    sound = loadSound("sound.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "STATUS: DETECTING OBJECTS";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    object = results;
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "STATUS: OBJECT DETECTED";
+            document.getElementById("number_of_objects").innerHTML = "NUMBER OF OBJECTS DETECTED ARE: " + object.length;
+            fill(r, g, b);
+            noFill();
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            stroke(r, g, b);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+            if (object[i].label == "person") {
+                document.getElementById("number_of_objects").innerHTML = "BABY FOUND";
+                sound.stop();
+            } else {
+                document.getElementById("number_of_objects").innerHTML = "BABY NOT FOUND";
+                sound.play();
+            }
+        }
+        if (object.length == 0) {
+            document.getElementById("number_of_objects").innerHTML = "BABY NOT FOUND";
+            sound.play();
+        }
+    }
+}
